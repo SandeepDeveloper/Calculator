7551f6d700091ec71037f551762df94af8811e08
Sandeep Verma <sandeepverma@sandeeps-macbook-air.local>
2017-04-26 15:55:03 +0530

    Initial Commit

---
+++ Auto Layout Testing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Auto Layout Testing
+//
+//  Created by Sandeep Verma on 26/04/17.
+//  Copyright © 2017 Code Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Auto Layout TestingTests/Auto_Layout_TestingTests.m
@@ -0,0 +1,39 @@
+//
+//  Auto_Layout_TestingTests.m
+//  Auto Layout TestingTests
+//
+//  Created by Sandeep Verma on 26/04/17.
+//  Copyright © 2017 Code Makers. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Auto_Layout_TestingTests : XCTestCase
+
+@end
+
+@implementation Auto_Layout_TestingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
